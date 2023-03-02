81631c60a4efc7c43c30c3a979441f6ea64f6e38
Scrobits Technologies LLP <73216435+scrobits-github@users.noreply.github.com>
2023-03-02 23:19:26 +0530

    add babelrc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Add the Babel loader for JavaScript and JSX files
+      config.module.rules.push({
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      });
+    }
+    return config;
+  },
+};
