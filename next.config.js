edcddaa789f59fbe0cab4abebaad994068dcd478
Scrobits Technologies LLP <73216435+scrobits-github@users.noreply.github.com>
2023-03-02 23:51:16 +0530

    add eslint

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Add the Babel loader for JavaScript and JSX files
-      config.module.rules.push({
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      });
-    }
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
